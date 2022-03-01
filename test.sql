675a20d6643c75f1c3afe27f13adf47f1096d288
lacizeidler <laci.zeidler@gmail.com>
2022-03-01 13:26:54 -0600

    list of favorites sellers

--- test.sql
+++ test.sql
@@ -1,7 +1,5 @@
-select o.id, u.first_name || ' ' || u.last_name as full_name, o.created_on, SUM(p.price) as total_price
-from bangazon_api_order o
-join auth_user u on o.user_id = u.id
-join bangazon_api_orderproduct op on o.id = op.order_id
-join bangazon_api_product p on op.product_id = p.id
-where o.completed_on is null
-group by op.order_id
+select u.first_name || ' ' || u.last_name as full_name, s.name
+from auth_user u
+join bangazon_api_favorite f on u.id = f.customer_id
+join bangazon_api_store s on f.store_id = s.id
+group by full_name
