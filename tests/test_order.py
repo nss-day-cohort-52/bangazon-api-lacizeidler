cb31a3148b7db717f2ecd36456e3272eb4a12b04
lacizeidler <laci.zeidler@gmail.com>
2022-03-02 10:42:55 -0600

    working on issue 13 test

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,47 +1,58 @@
-# from rest_framework import status
-# from rest_framework.test import APITestCase
-# from rest_framework.authtoken.models import Token
-# from django.core.management import call_command
-# from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from django.core.management import call_command
+from django.contrib.auth.models import User
 
-# from bangazon_api.models import Order, Product
+from bangazon_api.models import Order, Product
 
 
-# class OrderTests(APITestCase):
-#     def setUp(self):
-#         """
-#         Seed the database
-#         """
-#         call_command('seed_db', user_count=3)
-#         self.user1 = User.objects.filter(store=None).first()
-#         self.token = Token.objects.get(user=self.user1)
+class OrderTests(APITestCase):
+    def setUp(self):
+        """
+        Seed the database
+        """
+        call_command('seed_db', user_count=3)
+        self.user1 = User.objects.filter(store=None).first()
+        self.token = Token.objects.get(user=self.user1)
 
-#         self.user2 = User.objects.filter(store=None).last()
-#         product = Product.objects.get(pk=1)
+        self.user2 = User.objects.filter(store=None).last()
+        product = Product.objects.get(pk=1)
 
-#         self.order1 = Order.objects.create(
-#             user=self.user1
-#         )
+        self.order1 = Order.objects.create(
+            user=self.user1
+        )
 
-#         self.order1.products.add(product)
+        self.order1.products.add(product)
 
-#         self.order2 = Order.objects.create(
-#             user=self.user2
-#         )
+        self.order2 = Order.objects.create(
+            user=self.user2
+        )
 
-#         self.order2.products.add(product)
+        self.order2.products.add(product)
 
-#         self.client.credentials(
-#             HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'Token {self.token.key}')
 
-#     def test_list_orders(self):
-#         """The orders list should return a list of orders for the logged in user"""
-#         response = self.client.get('/api/orders')
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(len(response.data), 1)
+    def test_list_orders(self):
+        """The orders list should return a list of orders for the logged in user"""
+        response = self.client.get('/api/orders')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
 
-#     def test_delete_order(self):
-#         response = self.client.delete(f'/api/orders/{self.order1.id}')
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    def test_delete_order(self):
+        response = self.client.delete(f'/api/orders/{self.order1.id}')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-#     # TODO: Complete Order test
+    def test_complete_order(self):
+        url = 'api/orders/complete'
+        order = {
+            'created_on': '2022-02-28 15:21:38.231933',
+            'completed_on': '2022-02-29 15:21:38.231933',
+            'payment_type_id': 5
+        }
+        response = self.client.post(url, order, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["user"]['user'], self.token.user_id)
+        self.assertEqual(response.data["created_on"], order['created_on'])
+        self.assertEqual(response.data["completed_on"], order['completed_on'])
